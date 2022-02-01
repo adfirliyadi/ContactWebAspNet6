e51b18a204104dc5390dde52a1dd74f72f2896f5
Ading Firliyadi <firliyadi@gmail.com>
2022-02-01 14:44:30 +0700

    add models

---
+++ ContactWebModels/ContactManagerConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace ContactWebModels
+{
+    public class ContactManagerConstants
+    {
+        public const int MAX_STATE_NAME_LENGTH = 50;
+        public const int MAX_STATE_ABBR_LENGTH = 4;
+
+        public const int MAX_FIRST_NAME_LENGTH = 50;
+        public const int MAX_LAST_NAME_LENGTH = 250;
+        public const int MAX_EMAIL_LENGTH = 250;
+        public const int MAX_PHONE_LENGTH = 12;
+        public const int MAX_STREET_ADDRESS_LENGTH = 250;
+        public const int MAX_CITY_LENGTH = 120;
+        public const int MAX_ZIP_CODE = 10;
+        public const int MIN_ZIP_CODE = 5;
+    }
+}
---
+++ ContactWebModels/State.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactWebModels
+{
+    internal class State
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "State")]
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(ContactManagerConstants.MAX_STATE_NAME_LENGTH)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Abbreviation is required")]
+        [StringLength(ContactManagerConstants.MAX_STATE_ABBR_LENGTH)]
+        public string Abbreviation { get; set; }
+
+    }
+}
